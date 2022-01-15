305cc023addd03588184eed2fc394d07f8b8c623
VarunChoudhary35 <87988925+VarunChoudhary35@users.noreply.github.com>
2022-01-15 17:26:33 +0530

    dot added

--- src/ProductApp/ProductService.js
+++ src/ProductApp/ProductService.js
@@ -2,16 +2,16 @@ import axios from 'axios'
 const PRODUCT_API_URL = 'https://product-app-101.herokuapp.com/product';
 class ProductService {
     retrieveAllProducts(){
-         return axios.get(`${PRODUCT_API_URL}`);
+         return axios.get(`${PRODUCT_API_URL}.`);
     }
     deleteProduct(productId){
-        return axios.delete(`${PRODUCT_API_URL}/${productId}`)
+        return axios.delete(`${PRODUCT_API_URL}/${productId}.`)
     }
     addProduct(values){
-        return axios.post(`${PRODUCT_API_URL}`,values);
+        return axios.post(`${PRODUCT_API_URL}.`,values);
     }
     updateProduct(values){
-        return axios.put(`${PRODUCT_API_URL}`,values);
+        return axios.put(`${PRODUCT_API_URL}.`,values);
     }
 }
 export default new ProductService();
