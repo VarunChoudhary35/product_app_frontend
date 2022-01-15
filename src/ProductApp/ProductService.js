5436037d4aafd1cc24768e1b95b3f9e13e67331d
VarunChoudhary35 <87988925+VarunChoudhary35@users.noreply.github.com>
2022-01-15 17:36:17 +0530

    . removed header added

--- src/ProductApp/ProductService.js
+++ src/ProductApp/ProductService.js
@@ -2,16 +2,16 @@ import axios from 'axios'
 const PRODUCT_API_URL = 'https://product-app-101.herokuapp.com/product';
 class ProductService {
     retrieveAllProducts(){
-         return axios.get(`${PRODUCT_API_URL}.`);
+         return axios.get(`${PRODUCT_API_URL}`,{headers: {'Access-Control-Allow-Origin': 'true'});
     }
     deleteProduct(productId){
-        return axios.delete(`${PRODUCT_API_URL}/${productId}.`)
+        return axios.delete(`${PRODUCT_API_URL}/${productId}`,{headers: {'Access-Control-Allow-Origin': 'true'})
     }
     addProduct(values){
-        return axios.post(`${PRODUCT_API_URL}.`,values);
+        return axios.post(`${PRODUCT_API_URL}`,values,{headers: {'Access-Control-Allow-Origin': 'true'});
     }
     updateProduct(values){
-        return axios.put(`${PRODUCT_API_URL}.`,values);
+        return axios.put(`${PRODUCT_API_URL}`,values,{headers: {'Access-Control-Allow-Origin': 'true'});
     }
 }
 export default new ProductService();
